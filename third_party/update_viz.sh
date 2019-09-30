acc9b85bf46f3db3ce4367c8b25c9ea874e54801
limeng01 <limeng01@p1.com>
2019-09-30 12:56:36 +0800

    update viz

--- third_party/update_viz.sh
+++ third_party/update_viz.sh
@@ -4,15 +4,14 @@ set -eu
 set -o pipefail
 
 VIZ_VERSION="1.8.2"
-#VIZ_URL="https://unpkg.com/viz.js@${VIZ_VERSION}/viz.js"
-VIZ_URL="https://cdn.bootcss.com/viz.js/${VIZ_VERSION}/viz-lite.js"
+VIZ_URL="https://unpkg.com/viz.js@${VIZ_VERSION}/viz.js"
 
 cd $(dirname $0)
 
 VIZ_DIR=viz
 
 generate_viz_go() {
-    local viz_js=$(curl -s $VIZ_URL)
+    local viz_js=$(curl -s $VIZ_URL | sed 's/{{/{ {/g')
 
     cat <<-EOF > $VIZ_DIR/viz.go
 // viz.js is a JavaScript library for putting Graphviz on the web.
